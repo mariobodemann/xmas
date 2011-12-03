44afda4680af4999972852c33a44be40b0633efc
Mario Bodemann <mario.bodemann@web.de>
2011-12-03 15:05:33 +0100

    hello led working\!

--- led/led.c
+++ led/led.c
@@ -4,32 +4,18 @@
 
 #define F_CPU 16000000
 #include <avr/io.h>
-//#include <util/delay.h>
-
+#include <util/delay.h>
+#include <avr/interrupt.h>
+ 
 int main (void)
 {
-   DDRB = 0x01; //set up pin 0 on port B
+   DDRB = 0xFF; //set up all pins on port B
    while (1) //loop forever
    {
-       PORTB = 0x01; //set pin 0 on port B high
-       PORTB = 0x01; //set pin 0 on port B high
-       PORTB = 0x01; //set pin 0 on port B high
-       PORTB = 0x01; //set pin 0 on port B high
-       PORTB = 0x01; //set pin 0 on port B high
-       PORTB = 0x01; //set pin 0 on port B high
-       PORTB = 0x01; //set pin 0 on port B high
-       PORTB = 0x01; //set pin 0 on port B high
+       PORTB = 0xFF; //set pin 0 on port B high
+       _delay_ms(500);
        PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
-       PORTB = 0x00; //set pin 0 on port B low
+       _delay_ms(500);
    }
    return 0;
 }
