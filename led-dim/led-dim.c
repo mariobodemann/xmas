2652d17590c5da4df09befd3d9b6112e45c61ff6
Mario Bodemann <mario.bodemann@web.de>
2011-12-03 16:45:26 +0100

    added "buckle"-function for dimming of leds (sinus)

--- led-dim/led-dim.c
+++ led-dim/led-dim.c
@@ -8,8 +8,10 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define LED_MIN_DELAY 5
-#define LED_STEPS 1000
+#include "/usr/avr/include/math.h"
+
+#define LED_MIN_DELAY 10
+#define LED_STEPS 500
 
 void show_led(float brightness)
 {
@@ -19,21 +21,39 @@ void show_led(float brightness)
 	_delay_ms( brightness * LED_MIN_DELAY );
 }
 
+float dim_sin(float f)
+{
+	return sinf((f-.5)*M_PI)*0.5f+0.5f;
+}
+
+float dim_lin(float f)
+{
+	return f;
+}
+
+#define dim dim_sin
+
 int main (void)
 {
 	float increment = 1.0f / LED_STEPS;
 	DDRB = 0xFF; //set up all pins on port B
+	
 	while (1) //loop forever
 	{
-		for( float f= 0.0f; f < 1.0f + increment; f+= increment)
+		for( float f= increment; f < 1.0f + increment; f+= increment)
 		{
-			show_led(f);
+			show_led(dim(f));
 		}
+		_delay_ms(LED_MIN_DELAY);
 
-		for( float f= 0.0f; f < 1.0f + increment; f+= increment)
+		for( float f= increment; f < 1.0f + increment; f+= increment)
 		{
-			show_led(1.0f - f);
+			show_led(dim(1.0f - f));
 		}
+		
+		// set dark leds
+		PORTB = 0x0;
+		_delay_ms(LED_MIN_DELAY);
    	}
    	return 0;
 }
